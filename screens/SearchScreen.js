79e5bf65f995fc6d1bed8cc473de34c14ee9d487
soylu <21346670+dilarasoylu@users.noreply.github.com>
2019-11-16 15:37:58 -0800

    Add filters

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { Platform, View, Text, Button, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import { SearchBar } from 'react-native-elements';
+import { Ionicons } from '@expo/vector-icons';
 
 import Colors from '../constants/Colors';
 
 export default class SearchScreen extends Component {
   state = {
     searchQuery: '',
+    isLoading: false,
+    showFilters: true
   };
 
   updateSearch = searchQuery => {
@@ -16,7 +19,21 @@ export default class SearchScreen extends Component {
      });
   };
 
+  updateFilterVisibility = () => {
+    this.setState({
+      showFilters: !this.state.showFilters
+     });
+  };
+
   render() {
+    let filtersSection;
+
+    if (this.state.showFilters) {
+      filtersSection = getFilterView(this.state)
+    } else {
+      filtersSection = null
+    }
+
     return (
       <View style={styles.mainContainer}>
         <View style={styles.searchContainer}>
@@ -29,7 +46,16 @@ export default class SearchScreen extends Component {
             containerStyle={styles.searchOuterContainer}
             cancelButtonProps={styles.searchCancelButtonProps}
           />
+          <TouchableOpacity onPress={this.updateFilterVisibility}>
+            <Ionicons
+              name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+              size={27}
+              style={{ marginTop: 17 }}
+              color={this.state.showFilters ? Colors.tabIconDefault : Colors.tabIconSelected}
+            />
+          </TouchableOpacity>
         </View>
+        {filtersSection}
         <ScrollView style={styles.scrollViewContainer}>
           {/**
            * Go ahead and delete ExpoLinksView and replace it with your content;
@@ -43,6 +69,39 @@ export default class SearchScreen extends Component {
       </View>
     );
   }
+}
+
+function getFilterView(state) {
+  return (
+    <View>
+      {getFilterRow(state, 'Accessibility', ['Vision', 'Hearing', 'Attention'])}
+      {getFilterRow(state, 'Resource', ['Classroom', 'Article', 'Talk', 'People'])}
+      {getFilterRow(state, 'Subject', ['CS', 'ME', 'AA'])}
+    </View>
+  )
+}
+
+function getFilterRow(state, filter_type, filter_options) {
+  return (
+    <View style={styles.filterContainer}>
+      <Text style={styles.filterTextStyle}>{filter_type}:</Text>
+      <Button
+        title={filter_options[0]}
+        color={Colors.mainThemeColor}
+        onPress={() => Alert.alert('Vision pressed')}
+      />
+      <Button
+        title={filter_options[1]}
+        color={Colors.mainThemeColor}
+        onPress={() => Alert.alert('Vision pressed')}
+      />
+      <Button
+        title={filter_options[2]}
+        color={Colors.mainThemeColor}
+        onPress={() => Alert.alert('Vision pressed')}
+      />
+    </View>
+  )
 }
 
 SearchScreen.navigationOptions = {
@@ -59,13 +118,24 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   searchContainer: {
+    flexDirection: 'row',
     paddingHorizontal: 10
   },
   searchOuterContainer: {
-    backgroundColor: Colors.backgroundColor,
+    flex: 0.98,
+    backgroundColor: Colors.backgroundColor
   },
   searchCancelButtonProps: {
     color: Colors.mainThemeColor
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 20
+  },
+  filterTextStyle: {
+    fontSize: 18,
+    color: Colors.lightGray,
+    paddingTop: 8
   },
   scrollViewContainer: {
     paddingHorizontal: 20
