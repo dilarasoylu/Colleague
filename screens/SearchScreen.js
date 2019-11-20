734056f1556886eeaa351c35efe537d4c6a06b7a
soylu <21346670+dilarasoylu@users.noreply.github.com>
2019-11-20 14:25:17 -0800

    Fix typo

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -181,7 +181,7 @@ function anyTrue(dictionary) {
   return false
 }
 
-function shuffle(array) {
+function shuffle(a) {
     var j, x, i;
     for (i = a.length - 1; i > 0; i--) {
         j = Math.floor(Math.random() * (i + 1));
