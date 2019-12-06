5739ebf5077ff3b5449834d2b9877a5af0f77ea8
Daniela  Gonzalez <danielagonzalez@DN0a1f4ed3.SUNet>
2019-12-06 03:18:22 -0800

    fixed asserts for images

--- constants/Images.js
+++ constants/Images.js
@@ -2,8 +2,8 @@ const images = {
   red: require('../assets/images/red.jpeg'),
   logoBlue: require('../assets/images/astronaut.png'),
   uploadSuccess: require('../assets/images/uploadSuccess.png'),
-  // assignmentKarel: require('../assets/images/assignment-karel.jpg'),
-  // assignmentUI: require('../assets/images/assignment-ui.jpg')
+  assignmentKarel: require('../assets/images/assignment-karel.jpg'),
+  assignmentUI: require('../assets/images/assignment-ui.jpg')
 }
 
 export default images
